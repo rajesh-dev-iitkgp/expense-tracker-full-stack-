0175c5c18bdcbbbad2e46ce29fb988ca47ac57fb
Rajesh Kumar <252835131+rajesh-dev-iitkgp@users.noreply.github.com>
2026-05-07 10:51:07 +0530

    Adding protected routes for the pages in App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,17 +3,20 @@ import { Routes, Route } from 'react-router-dom'
 import Summary from './pages/Summary/Summary'
 import History from './pages/History/History'
 import AddTransaction from './pages/AddTransaction/AddTransaction'
-import Sidebar from './components/Sidebar/Sidebar'
+import ProtectedLayout from './components/ProtectedLayout/ProtectedLayout'
+import Login from './components/Login/Login'
 
 const App = () => {
 
   return (
     <BrowserRouter>
-      <Sidebar />
       <Routes>
-        <Route path="/" element={<Summary />} />
-        <Route path='/history' element={<History />} />
-        <Route path='/addTransaction' element={<AddTransaction />} />
+        <Route path="/login" element={<Login />} />
+        <Route element={<ProtectedLayout />}>
+          <Route path="/" element={<Summary />} />
+          <Route path='/history' element={<History />} />
+          <Route path='/addTransaction' element={<AddTransaction />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   )
