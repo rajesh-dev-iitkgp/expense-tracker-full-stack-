cdee10cb63e4a99aeeeb31ca958776e60c98e12f
Rajesh Kumar <252835131+rajesh-dev-iitkgp@users.noreply.github.com>
2026-05-07 11:16:22 +0530

    Handling Logic for Login component

--- frontend/src/components/Login/Login.jsx
+++ frontend/src/components/Login/Login.jsx
@@ -1,20 +1,49 @@
 import "./Login.css"
 import { useState } from "react"
+import { useNavigate, Navigate } from "react-router-dom"
+
 
 const Login = () => {
 
+    const navigate=useNavigate();
+
     const [currState,setCurrState]=useState("Login");
+    const [data,setData]=useState({
+        name:"",
+        email:"",
+        password:""
+    });
+
+    if (localStorage.getItem("isLoggedIn")) {
+        return <Navigate to="/" />
+    }
+
+    const onChangeHandler = (e)=>{
+        const name=e.target.name;
+        const value=e.target.value;
+        setData((prev) => ({
+            ...prev,
+            [name]: value
+        }));
+    }
+
+    const onSubmitHandler = (e)=>{
+        e.preventDefault();
+        localStorage.setItem("isLoggedIn",true);
+        navigate("/",{replace:true});
+        
+    }
 
   return (
     <div className="login">
-      <form className="login-container">
+      <form className="login-container" onSubmit={onSubmitHandler}>
         <div className="login-title">
             <h2>{currState}</h2>
         </div>
         <div className="login-inputs">
-            {currState==="Login"? <></> : <input type="text" name="name" placeholder="Your Name" required/>}
-            <input type="email"  name="email" placeholder="Your Email" required/>
-            <input type="password" name="password" placeholder="Password" required />
+            {currState==="Login"? <></> : <input type="text" name="name" onChange={onChangeHandler} value={data.name} placeholder="Your Name" required/>}
+            <input type="email"  name="email" onChange={onChangeHandler} value={data.email} placeholder="Your Email" required/>
+            <input type="password" name="password" onChange={onChangeHandler} value={data.password} placeholder="Password" required />
         </div>
         <button type="submit">{currState==="Sign Up" ? "Create Account" : "Login"}</button>
         <div className="login-condition">
