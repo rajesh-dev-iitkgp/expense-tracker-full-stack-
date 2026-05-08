e3b167b9dd0f7d93ebb38bd39b408eed36052b60
Rajesh Kumar <252835131+rajesh-dev-iitkgp@users.noreply.github.com>
2026-05-08 10:22:19 +0530

    Implemented addTransacton controller

--- backend/controllers/transactionController.js
+++ backend/controllers/transactionController.js
@@ -1,5 +1,24 @@
-const addTransaction = async (req, res) => {
+import transactionModel from "../models/transactionModel.js"
 
+const addTransaction = async (req, res) => {
+    try {
+
+        const newTransaction = new transactionModel({
+            userId: req.userId,
+            amount: req.body.amount,
+            type: req.body.type,
+            category: req.body.category,
+            date: req.body.date,
+            description: req.body.description
+        })
+
+        await newTransaction.save()
+        res.status(201).json({ message: "Transaction added successfully" })
+
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({ message: "Error in adding transaction" })
+    }
 }
 
 const editTransaction = async (req, res) => {
