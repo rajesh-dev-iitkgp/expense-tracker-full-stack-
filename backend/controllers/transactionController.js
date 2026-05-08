154ea71500ca2592cfc84d96faa7a7e79f87b2a1
Rajesh Kumar <252835131+rajesh-dev-iitkgp@users.noreply.github.com>
2026-05-08 11:02:40 +0530

    Implemented delete transaction controller

--- backend/controllers/transactionController.js
+++ backend/controllers/transactionController.js
@@ -44,7 +44,20 @@ const editTransaction = async (req, res) => {
 }
 
 const deleteTransaction = async (req, res) => {
-
+    try {
+        const deletedTransaction = await transactionModel.findOneAndDelete({
+        userId: req.userId,
+        _id: req.params.id
+    })
+        if(!deletedTransaction){
+            return res.status(404).json({ message: "Transaction not found" })
+        }
+        res.status(200).json({ message: "Transaction deleted successfully" })
+    } 
+    catch (error) {
+        console.log(error)
+        res.status(500).json({ message: "Error in deleting transaction" })
+    }
 }
 
 const getTransactions = async (req, res) => {
