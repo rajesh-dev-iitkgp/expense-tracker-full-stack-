6cfe3aaeab766112d9cd31a441f14d9dbc615d79
Rajesh Kumar <252835131+rajesh-dev-iitkgp@users.noreply.github.com>
2026-05-08 10:54:14 +0530

    Implemented edit transaction controller

--- backend/controllers/transactionController.js
+++ backend/controllers/transactionController.js
@@ -22,7 +22,25 @@ const addTransaction = async (req, res) => {
 }
 
 const editTransaction = async (req, res) => {
+    try {
+        const updatedTransaction = await transactionModel.findOneAndUpdate({
+            userId: req.userId,
+            _id: req.params.id
+        },
+        req.body,
+        {
+            new: true
+        })
+        if(!updatedTransaction){
+            return res.status(404).json({ message: "Transaction not found" })
+        }
+        res.status(200).json({ message: "Transaction edited successfully", transaction: updatedTransaction })
 
+    } 
+    catch (error) {
+        console.log(error)
+        res.status(500).json({ message: "Error in editing transaction" })
+    }
 }
 
 const deleteTransaction = async (req, res) => {
@@ -33,4 +51,4 @@ const getTransactions = async (req, res) => {
 
 }
 
-module.exports = { addTransaction, editTransaction, deleteTransaction, getTransactions }
+export { addTransaction, editTransaction, deleteTransaction, getTransactions }
--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -32,7 +32,7 @@ const loginUser = async (req,res)=>{
         console.log(error)
         return res.status(500).json({
             success: false,
-            message: "Error"
+            message: error.message
         })
     }
 }
