ff711bb569c7581cebf6b41100e2769e2d39ef19
Rajesh Kumar <252835131+rajesh-dev-iitkgp@users.noreply.github.com>
2026-05-08 11:34:47 +0530

    Connecting frontend transaction logic with backend

--- frontend/src/context/TransactionsContext.jsx
+++ frontend/src/context/TransactionsContext.jsx
@@ -1,35 +1,72 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable react-hooks/set-state-in-effect */
 /* eslint-disable react-refresh/only-export-components */
 import { useState,useEffect,createContext } from "react";
+import axios from "axios";
 export const TransactionsContext = createContext();
 
 const TransactionsProvider = ({ children }) => {
 
-    const [transactions, setTransactions] = useState(() => {
-        const stored = localStorage.getItem("transactions");
-        return stored ? JSON.parse(stored) : [];
-    });
+    const [transactions, setTransactions] = useState([]);
 
-    const addTransaction = (newTxn) => {
-        setTransactions(prev => [...prev, newTxn]);
-    };
-
-    const editTransaction = (updatedTxn) => {
-    setTransactions(prev =>
-        prev.map(txn =>
-        txn.id === updatedTxn.id ? { ...txn, ...updatedTxn } : txn
-        )
-      );
-    };
-
-    const deleteTransaction = (id) => {
-        setTransactions(prev =>
-            prev.filter(txn => txn.id !== id)
-        );
-    };
+    const fetchTransactions = async () => {
+        const token = localStorage.getItem("token");
+        if(token){
+            try {
+                const response = await axios.get("http://localhost:4000/api/transaction", {headers:{token}});
+                if(response.data.success){
+                    setTransactions(response.data.transactions);
+                    console.log(response.data.transactions)
+                }     
+            }
+             catch (error) {
+                console.log(error)
+            }
+        }
+    }
 
     useEffect(()=>{
-        localStorage.setItem("transactions", JSON.stringify(transactions));
-    },[transactions]);
+        fetchTransactions();
+    },[]);
+
+    const addTransaction = async (newTxn) => {
+        try {
+            const token = localStorage.getItem("token");
+            const response = await axios.post("http://localhost:4000/api/transaction",newTxn,{headers:{token}})
+            if(response.data.success){
+                fetchTransactions()
+            }
+        } 
+        catch (error) {
+            console.log(error)
+        }
+    };
+
+    const editTransaction = async (updatedTxn) => {
+        try {
+            const token = localStorage.getItem("token");
+            const response = await axios.put(`http://localhost:4000/api/transaction/${updatedTxn._id}`,updatedTxn,{headers:{token}})
+            if(response.data.success){
+                fetchTransactions()
+            }
+        } 
+        catch (error) {
+            console.log(error)
+        }
+    };
+
+    const deleteTransaction = async (id) => {
+        try {
+            const token = localStorage.getItem("token");
+            const response = await axios.delete(`http://localhost:4000/api/transaction/${id}`,{headers:{token}})
+            if(response.data.success){
+                fetchTransactions()
+            }
+        } 
+        catch (error) {
+            console.log(error)
+        }
+    };
 
     return (
     <TransactionsContext.Provider value={{transactions, addTransaction, editTransaction, deleteTransaction}}>
