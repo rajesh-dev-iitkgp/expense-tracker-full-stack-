03e17e72ebf41c6e573a7307b97a78f357b5e76e
Rajesh Kumar <252835131+rajesh-dev-iitkgp@users.noreply.github.com>
2026-05-08 09:53:21 +0530

    Creating Transaction Model for database

---
+++ backend/models/transactionModel.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const transactionSchema = new mongoose.Schema({
+    userId:{type: String,required: true},
+    amount:{type: Number,required: true},
+    type:{type: String,required: true},
+    category:{type: String,required: true},
+    date:{type: String,required: true},
+    description:{type: String,required: true}
+})
+
+const transactionModel = mongoose.models.transactions || new mongoose.model("transactions",transactionSchema)
+
+export default transactionModel
